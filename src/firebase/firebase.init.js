00d3303fc75abb97cf84447ff11eaae47e2ac100
Shoriful <shorifulbba0360@gmail.com>
2024-12-04 18:27:51 +0600

    added environment variable

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDOWpS5miKtI4Qz9YNkwgBv3ID_6GXXv5M",
-  authDomain: "visa-navigator-36de8.firebaseapp.com",
-  projectId: "visa-navigator-36de8",
-  storageBucket: "visa-navigator-36de8.firebasestorage.app",
-  messagingSenderId: "494242607346",
-  appId: "1:494242607346:web:7e99d9c3f5d927ce38e06b"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase

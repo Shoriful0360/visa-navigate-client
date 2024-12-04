3f650bb6920509143b7b7d353c771642ec739e35
Shoriful <shorifulbba0360@gmail.com>
2024-12-04 22:30:00 +0600

    added add visa form

--- src/pages/AddVisa.jsx
+++ src/pages/AddVisa.jsx
@@ -1,8 +1,158 @@
 
 const AddVisa = () => {
+
+    const handleBack = () => {
+
+    }
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault()
+        const form=e.target;
+        const name=form.name.value;
+        const img=form.img.value;
+        const visaType=form.visaType.value;
+        const processingTime=form.processingTime.value
+        const description=form.description.value
+        const age=form.age.value;
+        const fee=form.fee.value;
+        const validity=form.validity.value;
+        const application=form.application.value;
+        const option1=form.option1.value;
+        const option2=form.option2.value;
+   const visaForm={name,img,visaType,processingTime,description,age,fee,validity,application}
+   console.log(visaForm)
+        
+     
+    }
     return (
-        <div>
-           add visa 
+        <div className='bg-[#F4F3F0] p-10'>
+            <div className="">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+
+                    <div className="card w-full shrink-0 shadow-2xl">
+                        <form onSubmit={handleSubmit} className="card-body">
+                            <div className='md:flex   gap-4'>
+                                <div className=" w-1/2">
+                                    <label className="label">
+                                        <span className="label-text">Country Name</span>
+                                    </label>
+                                    <input type="text" name='name' placeholder="Enter your coffe name" className="input input-bordered w-full " required />
+                                </div>
+                                <div className="w-1/2">
+                                    <label className="label">
+                                        <span className="label-text">Country Image</span>
+                                    </label>
+                                    <input type="text" name='img' placeholder="Enter your coffee cheif" className="input w-full input-bordered" required />
+
+                                </div>
+                            </div>
+                            <div className='md:flex   gap-4'>
+                                <div className=" w-1/2">
+                                    <label className="label">
+                                        <span className="label-text">Visa Type</span>
+                                    </label>
+                                    {/* <input type="text" name='supplier' placeholder="Enter coffee supplier" className="input input-bordered w-full " required /> */}
+                                    <select name="visaType" className="select  input-bordered  w-full ">
+                                        <option disabled selected>What is the best visa</option>
+                                        <option>Tourist visa</option>
+                                        <option>Student visa</option>
+                                        <option>Official visa</option>
+                                        <option>JOb visa</option>
+                                    </select>
+                                </div>
+                                <div className="w-1/2">
+                                    <label className="label">
+                                        <span className="label-text">Processing Time</span>
+                                    </label>
+                                    <input type="text" name='processingTime' placeholder="Enter Processing Time" className="input w-full input-bordered" required />
+
+                                </div>
+                            </div>
+                            <div className='md:flex   gap-4'>
+                                <div className=" w-1/2">
+                                    <label className="label">
+                                        <span className="label-text">Description</span>
+                                    </label>
+                                    <textarea
+                                    name="description"
+                                        placeholder="Description"
+                                        className="textarea textarea-bordered textarea-xs w-full "></textarea>
+                                </div>
+                                <div className="w-1/2">
+                                    <label className="label">
+                                        <span className="label-text">Age</span>
+                                    </label>
+                                    <input type="number" name='age' placeholder="Enter your age year" className="input w-full input-bordered" required />
+
+                                </div>
+                            </div>
+                            <div className='md:flex gap-4'>
+                                <div className="w-1/2">
+                                    <label className="label">
+                                        <span className="label-text">Fee</span>
+                                    </label>
+                                    <input type="number" name='fee' placeholder="Enter photo URL" className="input w-full input-bordered" required />
+                                  
+
+                                </div>
+                                <div className="w-1/2">
+                                    <label className="label">
+                                        <span className="label-text">Validity</span>
+                                    </label>
+                                    <input type="text" name='validity' placeholder="Enter Validity" className="input w-full input-bordered" required />
+
+                                </div>
+                            </div>
+                            <div className='md:flex gap-4'>
+                                <div className="w-1/2">
+                                    <label className="label">
+                                        <span className="label-text">Application Method</span>
+                                    </label>
+                                    <input type="number" name='application' placeholder="Enter your application method" className="input w-full input-bordered" required />
+                                  
+
+                                </div>
+                                <div className="w-1/2">
+                                    <label className="label">
+                                        <span className="label-text">Required Document</span>
+                                    </label>
+                                    {/* <input type="text" name='validity' placeholder="Enter Validity" className="input w-full input-bordered" required /> */}
+                                  <div  id="checkboxForm">
+                                  <label>
+      <input type="checkbox" name="option1" value=" Valid Password" />
+      Valid Password
+    </label>
+    <label>
+      <input type="checkbox" name="option2" value="Visa application " />
+  Visa application
+    </label>
+    <label>
+      <input type="checkbox" name="option3" value="Recent passport-sized photograph" />
+      Recent passport-sized photograph
+    </label>
+    <label>
+      <input type="checkbox" name="option4" value=" Travel itinerary" />
+      Travel itinerary
+    </label>
+    <label>
+      <input type="checkbox" name="option5" value='  Proof of financial means' />
+      Proof of financial means
+    </label>
+
+                                  </div>
+                                </div>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn bg-[#D2B48C]">Add visa</button>
+                            </div>
+                        </form>
+                        <div className='flex justify-center'>
+                            <button onClick={handleBack} className='btn bg-secondary '>Back</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/MyAddedVisa.jsx
+++ src/pages/MyAddedVisa.jsx
@@ -1,10 +1,89 @@
 
 
 const MyAddedVisa = () => {
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+    }
     return (
-        <div>
-           my add visa 
+        <div className='bg-[#F4F3F0] p-10'>
+        <div className="">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+
+                <div className="card w-full shrink-0 shadow-2xl">
+                    <form onSubmit={handleSubmit} className="card-body">
+                        <div className='md:flex   gap-4'>
+                            <div className=" w-1/2">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name='name' placeholder="Enter your coffe name" className="input input-bordered w-full " required />
+                            </div>
+                            <div className="w-1/2">
+                                <label className="label">
+                                    <span className="label-text">chef</span>
+                                </label>
+                                <input type="text" name='chef' placeholder="Enter your coffee cheif" className="input w-full input-bordered" required />
+
+                            </div>
+                        </div>
+                        <div className='md:flex   gap-4'>
+                            <div className=" w-1/2">
+                                <label className="label">
+                                    <span className="label-text">Supplier</span>
+                                </label>
+                                <input type="text" name='supplier' placeholder="Enter coffee supplier" className="input input-bordered w-full " required />
+                            </div>
+                            <div className="w-1/2">
+                                <label className="label">
+                                    <span className="label-text">Taste</span>
+                                </label>
+                                <input type="text" name='taste' placeholder="Enter coffee taste" className="input w-full input-bordered" required />
+
+                            </div>
+                        </div>
+                        <div className='md:flex   gap-4'>
+                            <div className=" w-1/2">
+                                <label className="label">
+                                    <span className="label-text">Category</span>
+                                </label>
+                                <input type="text" name='category' placeholder="Enter coffee category" className="input input-bordered w-full " required />
+                            </div>
+                            <div className="w-1/2">
+                                <label className="label">
+                                    <span className="label-text">Details</span>
+                                </label>
+                                <input type="text" name='details' placeholder="Enter coffee-details" className="input w-full input-bordered" required />
+
+                            </div>
+                        </div>
+                       <div className='md:flex gap-4'>
+                       <div className="w-1/2">
+                            <label className="label">
+                                <span className="label-text">photo</span>
+                            </label>
+                            {/* <input type="text" name='photoUrl' placeholder="Enter photo URL" className="input w-full input-bordered" required /> */}
+                            <input type="file" name='photoUrl'  placeholder="Enter photo URL" className="file-input w-full max-w-xs" />
+
+                        </div>
+                        <div className="w-1/2">
+                            <label className="label">
+                                <span className="label-text">price</span>
+                            </label>
+                            <input type="text" name='price' placeholder="Enter Price" className="input w-full input-bordered" required />
+
+                        </div>
+                       </div>
+                        <div className="form-control mt-6">
+                            <button className="btn bg-[#D2B48C]">Add coffee</button>
+                        </div>
+                    </form>
+                 <div className='flex justify-center'>
+                 <button  className='btn bg-secondary '>Back</button>
+                 </div>
+                </div>
+            </div>
         </div>
+    </div>
     );
 };
 
